b7afdccae2acf6ff0de7b1bd6d5113a07ee73a82
Ravijnv7 <manohar0444@gmail.com>
2023-09-13 10:18:02 +0530

    added the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+const Navbar =()=>{
+    return (
+     <div className="heading">
+       <div className="logo-container">
+         <img className="logo" src="https://photos.wellfound.com/startups/i/9254137-a3c818fd13f3178dd2ca40d34a9b72c7-medium_jpg.jpg?buster=1684824309"/>
+         <div className="desc">
+        <p>Koach</p>
+        <h3>Borrow and Get Solutions for your roadblocks from Pro Minds</h3>
+       </div>
+       </div>
+       
+       <div className="nav-items">
+         <ul>
+           <li> <a href="url">Home</a></li>
+           <li> <a href="url">About us</a></li>
+           <li> <a href="url">Contact us</a></li>
+           <li> <a href="url">Cart</a></li>
+         </ul>
+       </div>
+     </div>
+     
+   )
+ }
+ export default Navbar;
